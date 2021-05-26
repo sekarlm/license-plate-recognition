ae78aee5cd53ce7149118e6d4920329da03625bb
sekarlm <larasatisekar9@gmail.com>
2021-05-26 08:34:47 +0700

    update database query

--- object_detection_api/app.py
+++ object_detection_api/app.py
@@ -14,14 +14,43 @@ import json
 app = Flask(__name__)
 
 # Database configuration
-# need to be configured
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/license_plate'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret-key'
 
 db = SQLAlchemy(app)
 
-from models import Plate
+# Database model
+class Plate(db.Model):
+    __tablename__ = 'plate_numbers'
+
+    id_user = db.Column(db.Integer)
+    id_transaction = db.Column(db.Integer, primary_key=True)
+    plate_number = db.Column(db.String(8), unique=True)
+    place = db.Column(db.String(64))
+    time_enter = db.Column(db.DateTime(timezone=True))
+    time_out = db.Column(db.DateTime(timezone=True))
+    price = db.Column(db.Numeric())
+
+    def __init__(self, id_user, id_transaction, plate_number, place):
+        self.id_user = id_user
+        self.id_transaction = id_transaction
+        self.plate_number = plate_number
+        self.place = place
+
+    def __repr__(self) -> str:
+        return '<id_transaction {}>'.format(self.id_transaction)
+
+    def serialize(self):
+        return {
+            'id_user': self.id_user,
+            'id_transaction': self.id_transaction,
+            'plate_number': self.plate_number,
+            'place': self.place,
+            'time_enter': self.time_enter,
+            'time_out': self.time_out,
+            'price': self.price
+        }
 
 # API that returns image with detections on it
 @app.route('/image', methods=['POST'])
@@ -76,17 +105,17 @@ def get_image():
 
     print("digits detected: ", digit_plate)
 
-    plate_exist = Plate.query.filter_by(plate_number=digit_plate).first()
+    plate_exist = db.session.query(Plate.id_user).filter_by(plate_number=digit_plate).scalar() is not None
 
     if plate_exist:
-        # kirim post
-    else:
-        # kirim error post
-
-    try:
         return jsonify({"response": digit_plate}), 200
-    except FileNotFoundError:
+    else:
         abort(404)
+
+    # try:
+    #     return jsonify({"response": digit_plate}), 200
+    # except FileNotFoundError:
+    #     abort(404)
 
 
 if __name__ == '__main__':
