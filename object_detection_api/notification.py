af16ee64637fbfd26092235711de7e5a30585782
sekarlm <larasatisekar9@gmail.com>
2021-06-01 15:53:05 +0700

    update config

--- object_detection_api/notification.py
+++ object_detection_api/notification.py
@@ -5,9 +5,9 @@ from io import StringIO
 import json
 import sys
 
-FCM_URL = 'https://fcm.googleapis.com/fcm/send'
-AUTH_KEY = 'key=AAAAqwEcUuY:APA91bG_UTv4R0QS4lvGcKSdsCOa2Rd0rZQci03nRHa9FgN9u3us8bupo2DWNzXmCLqqHo-pa-XjYjzlMPQI-lQug-mNKKWeOn70niTAGqPYAH2YySrV3FCzJgkNppbTUOK2W40ar6Mp'
-DEVICE_KEY = 'etk7hgw9Q9OxuMbh3qNezw:APA91bEcwEMEc-839HwY5NMXPm_w_sXE--LULRL4uxKq1gUlMvmtOl_SGZQrxxNdAeelYhC50JjB-lcI_YO3bH-KV9MtzzzFcREmd-YT1j3vVjOrkvelKlRdL0YNqkqeGFnNNoM3l2Od'
+FCM_URL = 'your-fcm-url'
+AUTH_KEY = 'your-auth-key'
+DEVICE_KEY = 'your-device-key'
 
 def send_notification(notif_data):
     
