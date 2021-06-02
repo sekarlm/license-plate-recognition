fdfd08bc54192784156bbd0bd64afdbe10eae919
sekarlm <larasatisekar9@gmail.com>
2021-06-02 17:58:41 +0700

    uncomment import

--- object_detection_api/app.py
+++ object_detection_api/app.py
@@ -2,12 +2,12 @@ import os
 import datetime
 from math import ceil, floor
 
-# import core.detection_tf as dtf
-# import cv2
-# import numpy as np
-# from object_detection.utils import label_map_util
-# from object_detection.utils import visualization_utils as viz_utils
-# import tensorflow as tf
+import core.detection_tf as dtf
+import cv2
+import numpy as np
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+import tensorflow as tf
 
 from flask import Flask, request, Response, jsonify, send_from_directory, abort, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -41,62 +41,62 @@ def get_image():
         # Unpack request
         image = request.files["images"]
         IMAGE_REQUEST = image.filename
-        # image.save(os.path.join(os.getcwd(), 'detections', 'tmp', IMAGE_REQUEST))
-        # IMAGE_PATH = os.path.join(os.getcwd(), 'detections', 'tmp', IMAGE_REQUEST)
+        image.save(os.path.join(os.getcwd(), 'detections', 'tmp', IMAGE_REQUEST))
+        IMAGE_PATH = os.path.join(os.getcwd(), 'detections', 'tmp', IMAGE_REQUEST)
         place = dict(request.form)["place"]
 
-        # category_index = label_map_util.create_category_index_from_labelmap('./Tensorflow/annotations/label_map.pbtxt')
+        category_index = label_map_util.create_category_index_from_labelmap('./Tensorflow/annotations/label_map.pbtxt')
 
-        # # Detect license plate object
-        # command = "python detect.py --images ./detections/tmp/{} ".format(IMAGE_REQUEST)
-        # os.system(command)
+        # Detect license plate object
+        command = "python detect.py --images ./detections/tmp/{} ".format(IMAGE_REQUEST)
+        os.system(command)
 
-        # IMAGE_CROPPED = os.path.join(os.getcwd(), 'detections', 'crop', 'license_plate_1.png')
+        IMAGE_CROPPED = os.path.join(os.getcwd(), 'detections', 'crop', 'license_plate_1.png')
 
-        # # Detect digit license plate
-        # img = cv2.imread(IMAGE_CROPPED)
-        # image_np = np.array(img)
+        # Detect digit license plate
+        img = cv2.imread(IMAGE_CROPPED)
+        image_np = np.array(img)
 
-        # input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-        # detections = dtf.detect_fn(input_tensor)
+        input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+        detections = dtf.detect_fn(input_tensor)
 
-        # num_detections = int(detections.pop('num_detections'))
-        # detections = {key: value[0, :num_detections].numpy()
-        #               for key, value in detections.items()}
-        # detections['num_detections'] = num_detections
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy()
+                      for key, value in detections.items()}
+        detections['num_detections'] = num_detections
 
-        # # detection_classes should be ints.
-        # detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+        # detection_classes should be ints.
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
 
-        # label_id_offset = 1
-        # image_np_with_detections = image_np.copy()
-        # boxes = detections['detection_boxes']
-        # max_boxes_to_draw = 8
-        # scores = detections['detection_scores']
-        # class_idx = detections['detection_classes'] + label_id_offset
-        # min_score_thresh = 0.5
+        label_id_offset = 1
+        image_np_with_detections = image_np.copy()
+        boxes = detections['detection_boxes']
+        max_boxes_to_draw = 8
+        scores = detections['detection_scores']
+        class_idx = detections['detection_classes'] + label_id_offset
+        min_score_thresh = 0.5
 
-        # result = []
-        # for i in range(min(max_boxes_to_draw, boxes.shape[0])):
-        #     if scores[i] > min_score_thresh:
-        #         item = {"label": category_index[class_idx[i]]["name"],
-        #                 "score": scores[i],
-        #                 "boxes": boxes[i][1]
-        #                 }
-        #         result.append(item)
+        result = []
+        for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+            if scores[i] > min_score_thresh:
+                item = {"label": category_index[class_idx[i]]["name"],
+                        "score": scores[i],
+                        "boxes": boxes[i][1]
+                        }
+                result.append(item)
 
-        # result = sorted(result, key=lambda k: k['boxes'])
-        # digit_plate = ""
-        # for digit in result:
-        #     digit_plate = digit_plate + digit["label"]
-
-        digit_plate = "B2247XS"
+        result = sorted(result, key=lambda k: k['boxes'])
+        digit_plate = ""
+        for digit in result:
+            digit_plate = digit_plate + digit["label"]
 
         print("digits detected: ", digit_plate)
 
         # Filter query to database
         vehicle = db.session.query(Vehicle).filter_by(plate_number=digit_plate).scalar()
         transaction = db.session.query(Transaction).filter_by(plate_number=digit_plate, place=place, isDone=False).scalar()
+        print(vehicle is not None)
+        print(transaction is not None)
 
         # If user with plate_number <digit_plate> parking at <place> --> want to quit parking lot
         if (vehicle is not None) and (transaction is not None):
@@ -171,6 +171,7 @@ def get_image():
                 }
                 print("update transaction failed")
                 return render_template('parking.html', data=data)
+
         # If user with plate_number <digit_plate> is exist and want to parking at <place>
         elif (vehicle is not None):
             try:
@@ -223,6 +224,7 @@ def get_image():
                 }
                 print("add new transaction record failed")
                 return render_template('parking.html', data=data)
+
         # If user not found
         else:
             data = {
@@ -235,4 +237,4 @@ def get_image():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
